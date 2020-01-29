ef95b1c08911a24b518a9657435fa24db00e1295
Anthony <anthony@residentadvisor.net>
2020-01-29 21:59:28 +0000

    Reverting a change

--- src/tokens/employmentStatus.js
+++ src/tokens/employmentStatus.js
@@ -1,9 +1,7 @@
-const EmpStat = Object.freeze({
-  FULL_TIME: 1,
-  PART_TIME: 2,
-  STUDENT: 3
+const EmploymentStatus = Object.freeze({
+  FULL_TIME: 'Full time',
+  PART_TIME: 'Part time',
+  STUDENT: 'Student'
 })
-
-const EmploymentStatus = [{EmpStat.FULL_TIME 'Full time'}]
 
 export default EmploymentStatus
